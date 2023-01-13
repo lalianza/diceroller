2c4dd977261efad60ee931b16d04169a9899bd2c
luizlianza <lianzadafranca@gmail.com>
2023-01-13 00:52:22 -0300

    new values

--- app.py
+++ app.py
@@ -45,9 +45,9 @@ st.markdown("""Try this to build your D&D character,
 chose how many dices you will roll, how many drops and how many times,
 them see if you are lucky""")
 
-dices = st.slider(label="Number of dices to roll", min_value=1, max_value=100, step=1)
-drops = st.slider(label="Number of dices to drop", min_value=1, max_value=100, step=1, help="must be higher than rolled dices")
-times = st.slider(label="Number times to try", min_value=1, max_value=100, step=1)
+dices = st.slider(label="Number of dices to roll", min_value=1, max_value=20, step=1)
+drops = st.slider(label="Number of dices to drop", min_value=1, max_value=20, step=1, help="must be higher than rolled dices")
+times = st.slider(label="Number times to try", min_value=1, max_value=20, step=1)
 
 if st.button("get me some stats"):
     df_roll, final_roll = diceroller(dices, drops, times)
